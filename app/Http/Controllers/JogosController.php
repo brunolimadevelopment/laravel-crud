696be075d471dafc0698f04412eaceab71da44ac
Bruno Lima <brunolimadevelopment@gmail.com>
2024-02-27 17:26:43 -0300

    feat: how to work with controller

---
+++ app/Http/Controllers/JogosController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers; // Define o espaço de nomes ao qual a classe JogosController pertence.
+
+use Illuminate\Http\Request; // Permite que você importe classes em seu arquivo. Neste caso, está importando a classe Request do Laravel, que é usada para manipular as solicitações HTTP.
+
+class JogosController extends Controller
+{
+
+    public function index()
+    {
+        //dd('Olá mundo');
+
+        // Passando parâmetros para rota jogos, fica mais seguro via Controller do que via router.
+        $id = 1;
+        $nome = 'Bruno Lima';
+        return view('jogos', ['nome'=>$nome, 'id'=>$id]);
+    }
+}
