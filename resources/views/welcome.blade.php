139f57f77a0ea570542b2541a07fd62e0afbe934
Bruno Lima <brunolimadevelopment@gmail.com>
2024-02-27 16:32:20 -0300

    feat: how to work with routes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Bruno
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,41 @@
 |
 */
 
-Route::get('/', function () {
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//Route::view('/bruno', 'jogos');
+// Route::get('/jogos', function () {
+//     return "Curso de Laravel";
+// });
+//Route::view('/jogos', 'jogos', ['name'=>'Bruno']); - variavel
+
+// parametro dinamico - opcional apenas texto
+// Route::get('/jogos/{name?}', function($name = null) {
+//     return view('jogos', ['nomeJogo'=>$name]);
+// })->where('name', '[A-Za-z]+');
+
+// parametro dinamico - opcional apenas numero
+// Route::get('/jogos/{id?}', function($id = null) {
+//     return view('jogos', ['idJogo'=>$id]);
+// })->where('id', '[0-9]+');
+
+// parametro dinamico - juntando os dois exemplos anteriores
+// Route::get('/jogos/{id?}/{name?}', function($id = null, $name = null) {
+//     return view('jogos', ['id'=>$id, 'nome'=>$name]);
+// })->where(['id', '[0-9]+','name'=>'[a-z]+']);
+
+Route::get('/jogos', function () {
+    return view('jogos');
+});
+
+// redirecionando para outra rota - href="#"
+Route::get('/casa', function () {
     return view('welcome');
+})->name('home-index');
+
+// Erro de rotas - exibe uma mensagem de erro na tela.
+Route::fallback(function () {
+    return 'Erro ao localizar a rota!';
 });
